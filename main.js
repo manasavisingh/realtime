7e5a33bfdfce5486950e700f6923980ab5697a18
manasavisingh <79738892+manasavisingh@users.noreply.github.com>
2021-09-08 15:47:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightWristX = 0;
+leftWristX=0;
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+canvas=createCanvas(550,550);
+canvas.position(560,150);
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log('poseNet is intialized');
+}
+
+
+
+function draw(){
+    background('#ce92de');
+    document.getElementById("square_side").innerHTML="side of the square is "+difference+"px";
+    fill('#75426d');
+    stroke('#05f0ec');
+    square(noseX,noseY,difference);
+}
+function gotPoses(results){
+    if(results.length>0){
+console.log(results);
+noseX=results[0].pose.nose.x;
+noseY=results[0].pose.nose.y;
+console.log("noseX="+noseX+"noseY="+noseY) ;
+  leftWristX=results[0].pose.leftWrist.x;
+rightWristX=results[0].pose.rightWrist.x;
+difference=floor(leftWristX-rightWristX);
+console.log("leftWristX="+leftWristX+"rightWristX="+rightWristX); }
+}
